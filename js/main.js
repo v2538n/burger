4f023ffe7eb1c9fe8943b7233fc603a6c713fd09
v2538n <v2538n@gmail.com>
2021-04-04 05:27:31 +0300

    add webp

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 (function($){
 	$(document).ready(function(){
 
-		$('.burger').append("<span class='burger__element'></span>");
-
-		let btn = $('.burger, .top-nav');
-
-		btn.click(function(){
-			btn.toggleClass('active');
-			$('body').toggleClass('lock');
-		});
-
-		$('html, body').animate({
-			scrollTop: $('.s2').offset().top
-		});
+	
 
 	});
 })(jQuery);
